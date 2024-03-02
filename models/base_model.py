66a82bc17d3cc6305f9e34fbc94465a80a702224
sebteouii <sebteouiikhoubayb@gmail.com>
2024-03-02 15:11:49 +0100

    created a method to generate a dictionary representation of an instance (method to_dict()).

---
+++ models/base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+base_model module: Defines the BaseModel class.
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel class: Represents a base model with common attributes and methods
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "_class_":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel object
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the 'updated_at' attribute with the current
+        datetime and saves the changes to the storage.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Converts the BaseModel object to a dictionary for serialization.
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["_class_"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
