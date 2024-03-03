3ba8504783357845a198310c84a52584bb31703f
khalilBahi <7753@holbertonstudents.com>
2024-03-03 19:44:04 +0100

    Write a class amenity that inherits from BaseModel

---
+++ amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Amenity module: Contains the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class: Represents an amenity with a name."""
+    name = ""
+    
