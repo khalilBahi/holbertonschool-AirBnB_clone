3d219d05f8255b6b7674d55860553e47d2b38995
khalilBahi <7753@holbertonstudents.com>
2024-03-03 19:59:45 +0100

    Update FileStorage to manage correctly serialization and deserialization of all our new classes: Place, State, City, Amenity, user and Review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-    """
+    """Represent an abstracted storage engine."""
 
     __file_path = "file.json"
     __objects = {}
@@ -28,7 +34,7 @@ class FileStorage:
 
     def reload(self):
         """Deserializes the JSON file to instances in __objects."""
-        from models.base_model import BaseModel
+
         try:
             with open(self.__file_path, "r") as f:
                 for key, value in json.load(f).items():
