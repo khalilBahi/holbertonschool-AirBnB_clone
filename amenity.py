e69f3b6452930d1114f166537624f97f0e9cdc39
khalilBahi <146719853+khalilBahi@users.noreply.github.com>
2024-03-03 19:49:07 +0100

    Delete amenity.py

--- amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Amenity module: Contains the Amenity class."""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """Amenity class: Represents an amenity with a name."""
-    name = ""
-    
